1e706cf9d3beda83fb02ae059ef2ccd5125a9611
bccosta23 <vbcorporacao@gmail.com>
2025-01-30 07:44:02 -0300

    Primeiro projet 01

---
+++ script.js
@@ -0,0 +1,218 @@
+
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const cartItemsContainer = document.getElementById("cart-items")
+const cartTotal = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartCounter = document.getElementById("cart-count")
+const addressInput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+let cart = [];
+
+/* Abrir modal do carrinho */
+cartBtn.addEventListener("click", function(){
+    cartModal.style.display = "flex"
+    updateCartModal();
+})
+
+// Fechar modal do carrinho //
+cartModal.addEventListener("click", function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+})
+
+closeModalBtn.addEventListener("click", function(){
+    cartModal.style.display = "none"
+})
+
+
+menu.addEventListener("click", function(event){
+    //console.log(event.target)///
+    let parentButton = event.target.closest(".add-to-cart-btn")
+
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        addToCart(name, price)
+    }
+})
+
+// Funçao para add no carrinho //
+function addToCart(name, price){
+    const existingItem = cart.find(item => item.name === name)
+
+    if(existingItem){
+
+        existingItem.quantity += 1;
+    }else{
+
+        cart.push({
+         name,
+         price,
+         quantity: 1,
+
+        })
+
+    }
+
+    updateCartModal()
+    
+}
+
+//atualizar carrinho//
+function updateCartModal(){
+    cartItemsContainer.innerHTML = "";
+    let total = 0;
+
+    cart.forEach(item => {
+       const cartItemElement  = document.createElement("div");
+
+       cartItemElement.innerHTML = `
+            <div class="flex items-center justify-between">
+                <div>
+                 <p class="font-bold">${item.name}</p>
+                 <p>Qtd: ${item.quantity}</p>
+                 <p class="font-medium mt-2">R$: ${item.price.toFixed(2)}</p>
+                </div>
+
+                
+                <button class="remove-from-cart-btn" data-name="${item.name}">                     
+                   Remover 
+                </button>
+              
+            </div>
+        `
+
+        total += item.price * item.quantity;
+
+
+        cartItemsContainer.appendChild(cartItemElement)
+    })
+
+    cartTotal.textContent = total.toLocaleString("pt-BR",{
+        style: "currency",
+        currency: "BRL"
+    });
+
+    cartCounter.innerHTML = cart.length;
+
+}
+
+//Fução remover o item do carrinho//
+cartItemsContainer.addEventListener("click", function (event){
+    if(event.target.classList.contains("remove-from-cart-btn")){
+        const name = event.target.getAttribute("data-name")
+
+        removeItemCart(name);
+    }
+})
+
+function removeItemCart(name){
+    const index = cart.findIndex(item => item.name === name);
+
+    if(index !== -1){
+      const item = cart[index]
+
+      if(item.quantity > 1){
+        item.quantity -= 1;
+        updateCartModal();
+        return;
+      }
+
+      cart.splice(index, 1);
+      updateCartModal();
+    }
+}
+
+addressInput.addEventListener("input", function(event){
+    let inputValue = event.target.value;
+
+    if(inputValue !== ""){
+        addressInput.classList.remove("border-red-500");
+        addressWarn.classList.add("hidden")
+    }
+
+
+})
+
+
+//Finalizar Pedido
+checkoutBtn.addEventListener("click", function(){
+
+    const isOpen = checkRestaurantOpen();
+    if(!isOpen){
+         
+
+       Toastify({
+        text:  "🍦 Poxa, deu vontade de um cone agora? Infelizmente, estamos fechados! Mas fica tranquilo, logo abrimos das 18:00 às 22:00. Segura a vontade aí! 😅",
+        duration: 3000,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "#ef4444",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+
+
+
+        return;
+    }
+
+    
+    if(cart.length === 0) return;
+    if(addressInput.value === ""){
+       addressWarn.classList.remove("hidden")
+       addressInput.classList.add("border-red-500") 
+       return;   
+    }
+
+
+ 
+   // Enviar Pedido WhatsApp
+    const cartItems = cart.map((item) => {
+      return `${item.name} Quantidade: (${item.quantity}) Preço: R$${item.price} \n`
+    }).join("");
+
+    const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    const totalMessage = `Total: R$${total.toFixed(2)}\n`;
+
+    const message = encodeURIComponent(cartItems + "Endereço: " + addressInput.value + "\n" + totalMessage);
+    const phone = "82996086294";
+
+    window.open(`https://wa.me/${phone}?text=${message}`, "_blank");
+
+
+  cart = [];
+  updateCartModal();
+
+
+})
+
+
+//Verificar hora e manipular card horario
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora <22;
+    //true = loja está aberta 
+}
+
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500")
+    spanItem.classList.add("bg-green-500")
+}else{
+    spanItem.classList.remove("bg-green-500")
+    spanItem.classList.add("bg-red-500")
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.js'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        'sans':['Poppins','sans-serif']
+      },
+
+      minWidth: {
+        '600': '600px', // Define min-w-600
+      },
+
+      backgroundImage:{
+        "home":"url('/assets/bg.png')"
+      }       
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
